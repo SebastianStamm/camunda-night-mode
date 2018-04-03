3bdfdc682c5c704b959a28b6c42b2f27953ab1eb
Sebastian Stamm <sebastian.stamm@camunda.com>
2018-04-03 10:44:47 +0200

    add global scale factor for canvas generation

--- bpmn2Level.js
+++ bpmn2Level.js
@@ -1,15 +1,25 @@
-import 'https://unpkg.com/bpmn-js@0.27.6/dist/bpmn-viewer.development.js';
-import { getNodeFurthestAwayFrom, removeNodeFromGraph, isNodeReachable, dijkstra } from './levelgenerator.js';
+import "https://unpkg.com/bpmn-js@0.27.6/dist/bpmn-viewer.development.js";
+import {
+  getNodeFurthestAwayFrom,
+  removeNodeFromGraph,
+  isNodeReachable,
+  dijkstra
+} from "./levelgenerator.js";
+
+const scaleFactor = 0.4;
 
 (async () => {
-  const xml = await (await fetch('./bat.bpmn')).text();
+  const xml = await (await fetch("./bat.bpmn")).text();
   const viewer = new BpmnJS();
   viewer.importXML(xml, () => {
     const { min, max } = (reg => {
       const min = { x: Infinity, y: Infinity };
       const max = { x: -Infinity, y: -Infinity };
       reg.forEach(elem => {
-        if (elem.type !== 'label' && elem.businessObject.$instanceOf('bpmn:FlowNode')) {
+        if (
+          elem.type !== "label" &&
+          elem.businessObject.$instanceOf("bpmn:FlowNode")
+        ) {
           min.x = Math.min(min.x, elem.x);
           min.y = Math.min(min.y, elem.y);
           max.x = Math.max(max.x, elem.x + elem.width);
@@ -17,7 +27,7 @@ import { getNodeFurthestAwayFrom, removeNodeFromGraph, isNodeReachable, dijkstra
         }
       });
       return { min, max };
-    })(viewer.get('elementRegistry'));
+    })(viewer.get("elementRegistry"));
 
     const offset = {
       x: -min.x + 1,
@@ -25,31 +35,48 @@ import { getNodeFurthestAwayFrom, removeNodeFromGraph, isNodeReachable, dijkstra
     };
 
     const dimensions = {
-      x: max.x - min.x + 2,
-      y: max.y - min.y + 2
+      x: (max.x - min.x + 2) * scaleFactor,
+      y: (max.y - min.y + 2) * scaleFactor
     };
 
-    const canvas = document.createElement('canvas');
-    canvas.setAttribute('width', dimensions.x);
-    canvas.setAttribute('height', dimensions.y);
+    const canvas = document.createElement("canvas");
+    canvas.setAttribute("width", dimensions.x);
+    canvas.setAttribute("height", dimensions.y);
 
-    const ctx = canvas.getContext('2d');
+    const ctx = canvas.getContext("2d");
 
-    ctx.fillStyle = 'black';
+    ctx.fillStyle = "black";
     ctx.fillRect(0, 0, dimensions.x, dimensions.y);
 
-    ctx.fillStyle = 'white';
-    ctx.strokeStyle = 'white';
-    ctx.lineWidth = 10;
+    ctx.fillStyle = "white";
+    ctx.strokeStyle = "white";
+    ctx.lineWidth = 2;
 
-    viewer.get('elementRegistry').forEach(elem => {
-      if (elem.type !== 'label' && elem.businessObject.$instanceOf('bpmn:FlowNode')) {
-        ctx.fillRect(elem.x + offset.x, elem.y + offset.y, elem.width, elem.height);
+    viewer.get("elementRegistry").forEach(elem => {
+      if (
+        elem.type !== "label" &&
+        elem.businessObject.$instanceOf("bpmn:FlowNode")
+      ) {
+        ctx.fillRect(
+          (elem.x + offset.x) * scaleFactor,
+          (elem.y + offset.y) * scaleFactor,
+          elem.width * scaleFactor,
+          elem.height * scaleFactor
+        );
       }
-      if (elem.type !== 'label' && elem.businessObject.$instanceOf('bpmn:SequenceFlow')) {
-        ctx.moveTo(elem.waypoints[0].x + offset.x, elem.waypoints[0].y + offset.y);
+      if (
+        elem.type !== "label" &&
+        elem.businessObject.$instanceOf("bpmn:SequenceFlow")
+      ) {
+        ctx.moveTo(
+          (elem.waypoints[0].x + offset.x) * scaleFactor,
+          (elem.waypoints[0].y + offset.y) * scaleFactor
+        );
         elem.waypoints.slice(1).forEach(({ x, y }) => {
-          ctx.lineTo(x + offset.x, y + offset.y);
+          ctx.lineTo(
+            (x + offset.x) * scaleFactor,
+            (y + offset.y) * scaleFactor
+          );
         });
         ctx.stroke();
       }
@@ -57,32 +84,38 @@ import { getNodeFurthestAwayFrom, removeNodeFromGraph, isNodeReachable, dijkstra
 
     document.body.appendChild(canvas);
 
-
     // convert bpmn to graph
-    const nodes = viewer.get('elementRegistry').filter(e => e.type !== 'label' && e.businessObject && e.businessObject.$instanceOf('bpmn:FlowNode'));
+    const nodes = viewer
+      .get("elementRegistry")
+      .filter(
+        e =>
+          e.type !== "label" &&
+          e.businessObject &&
+          e.businessObject.$instanceOf("bpmn:FlowNode")
+      );
 
     const graph = {};
     nodes.forEach(node => {
       graph[node.id] = [];
-      node.businessObject.incoming && node.businessObject.incoming.forEach(sf => {
-        graph[node.id].push(sf.sourceRef.id);
-      });
-      node.businessObject.outgoing && node.businessObject.outgoing.forEach(sf => {
-        graph[node.id].push(sf.targetRef.id);
-      });
+      node.businessObject.incoming &&
+        node.businessObject.incoming.forEach(sf => {
+          graph[node.id].push(sf.sourceRef.id);
+        });
+      node.businessObject.outgoing &&
+        node.businessObject.outgoing.forEach(sf => {
+          graph[node.id].push(sf.targetRef.id);
+        });
     });
 
-    const start = nodes.find(e => e.businessObject.$instanceOf('bpmn:StartEvent')).id;
+    const start = nodes.find(e =>
+      e.businessObject.$instanceOf("bpmn:StartEvent")
+    ).id;
     const { dist } = dijkstra(graph, start);
 
     const goal = nodes
-      .filter(e => e.businessObject.$instanceOf('bpmn:EndEvent'))
-      .map(node => [
-        node.id,
-        dist[node.id]
-      ])
+      .filter(e => e.businessObject.$instanceOf("bpmn:EndEvent"))
+      .map(node => [node.id, dist[node.id]])
       .sort((a, b) => b[1] - a[1])[0][0];
-
 
     let subGoal = goal;
     let subGraph = graph;
@@ -99,7 +132,7 @@ import { getNodeFurthestAwayFrom, removeNodeFromGraph, isNodeReachable, dijkstra
       subGoal = keyRoom;
       i++;
     }
-    console.log('player should go from', start, 'to', goal);
+    console.log("player should go from", start, "to", goal);
     console.log(keyLocations, graph);
   });
 })();
--- index.js
+++ index.js
@@ -1,29 +1,33 @@
-import { getNodeFurthestAwayFrom, removeNodeFromGraph, isNodeReachable } from './levelgenerator.js';
+import {
+  getNodeFurthestAwayFrom,
+  removeNodeFromGraph,
+  isNodeReachable
+} from "./levelgenerator.js";
 
-import './bpmn2Level.js';
+import "./bpmn2Level.js";
 
-(async () => {
-  const graph = await (await fetch('./graph.json')).json();
+// (async () => {
+//   const graph = await (await fetch('./graph.json')).json();
 
-  const start = 0;
-  const goal = getNodeFurthestAwayFrom(graph, start);
+//   const start = 0;
+//   const goal = getNodeFurthestAwayFrom(graph, start);
 
-  let subGoal = goal;
-  let subGraph = graph;
+//   let subGoal = goal;
+//   let subGraph = graph;
 
-  const keyLocations = {};
+//   const keyLocations = {};
 
-  let i = 0;
-  while (isNodeReachable(subGraph, start, subGoal) && i < 100) {
-    const keyRoom = getNodeFurthestAwayFrom(subGraph, start, subGoal);
-    subGraph = removeNodeFromGraph(subGraph, subGoal);
+//   let i = 0;
+//   while (isNodeReachable(subGraph, start, subGoal) && i < 100) {
+//     const keyRoom = getNodeFurthestAwayFrom(subGraph, start, subGoal);
+//     subGraph = removeNodeFromGraph(subGraph, subGoal);
 
-    keyLocations[subGoal] = keyRoom;
+//     keyLocations[subGoal] = keyRoom;
 
-    subGoal = keyRoom;
-    i++;
-  }
+//     subGoal = keyRoom;
+//     i++;
+//   }
 
-  // console.log('player should go from', start, 'to', goal);
-  // console.log(keyLocations);
-})();
+//   // console.log('player should go from', start, 'to', goal);
+//   // console.log(keyLocations);
+// })();
